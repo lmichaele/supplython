d2a818538d6a33b57821da17395fe84cb717cc24
Edwards <luke.edwards@agcocorp.com>
2020-10-18 16:06:21 +1100

    let's see what breaks

--- invoices/views.py
+++ invoices/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+def home(request):
+	return render(request, 'home.html')
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
--- supplython/urls.py
+++ supplython/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from invoices import views
+
 urlpatterns = [
+	path('', views.home, name='home'),
+	path('accounts/', include('django.contrib.auth.urls')),
     path('invoices/', include('invoices.urls')),
     path('admin/', admin.site.urls),
 ]
